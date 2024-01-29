8e942badb469a25dd2682c8f1a009dc06eda9767
nandink <fernanda.cerda@sansano.usm.cl>
2024-01-28 23:18:59 -0300

    gitignore added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import postRoutes from './routes/posts.routes.js'
+import cors from 'cors'
+
+
 
 
 const app = express()
+app.use(cors())
 
 //middlewares
 app.use(express.json())
